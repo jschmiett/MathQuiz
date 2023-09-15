5da56704f73ac079779321d81d2ca89e272635a9
Jennifer Schmiett <bailliejm@gmail.com>
2023-09-15 11:56:16 -0700

    Debug global variables for checkboxes

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var gameProgress = 0;
 var timeLeft = 60;
 var timerEl = document.getElementById("countdown");
 var mainEl = document.getElementById("timerEl");
+var singleDigit = document.getElementById("singleDigit");
+var doubleDigit = document.getElementById("doubleDigit");
 
 document.getElementById("start").addEventListener("click", start);
 document.getElementById("digitsSelected").addEventListener("click", isChecked);
@@ -14,7 +16,17 @@ function start() {
 
 function isChecked() {
   console.log("isChecked is running");
-  if (document.getElementById("singleDigit").checked) {
-    console.log("Single Digit is checked");
+  console.log(singleDigit, singleDigit.checked);
+  console.log(doubleDigit, doubleDigit.checked);
+  if (singleDigit == true && doubleDigit == true) {
+    console.log("Single Digit and Double Digit are checked");
+  } else if (singleDigit == true && doubleDigit == false) {
+    console.log("Only Single Digit is selected");
+  } else if (singleDigit == false && doubleDigit == true) {
+    console.log("Only Double Digits is selected");
+  } else if (singleDigit == false && doubleDigit == false) {
+    console.log("Neither options is selected");
+  } else {
+    console.log("Something is very broken");
   }
 }
