ed616242657d664b58a76a65dcae2530b7eb4e9f
Jennifer Schmiett <bailliejm@gmail.com>
2023-09-14 18:06:53 -0700

    Setting up page flow triggered by buttons

--- script.js
+++ script.js
@@ -4,9 +4,17 @@ var timerEl = document.getElementById("countdown");
 var mainEl = document.getElementById("timerEl");
 
 document.getElementById("start").addEventListener("click", start);
+document.getElementById("digitsSelected").addEventListener("click", isChecked);
 
 function start() {
   console.log("Worked");
   document.getElementById("welcome").setAttribute("class", "hide");
-  document.getElementById("quizOptions").classList.remove("hide");
+  document.getElementById("digitOptions").classList.remove("hide");
+}
+
+function isChecked() {
+  console.log("isChecked is running");
+  if (document.getElementById("singleDigit").checked) {
+    console.log("Single Digit is checked");
+  }
 }
